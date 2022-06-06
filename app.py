5d4e7a14a2e66bcf386207757d33f5000bb1d01c
pluis9 <luispalcivar@gmail.com>
2022-06-06 00:34:26 -0500

    se agrega funcionalidad a pagina oferta

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 #Se importa la librería Flask, abort y render_template
-from flask import Flask, abort, render_template
+import re
+import json
+import os
+from markupsafe import escape
+from flask import Flask, abort, render_template, request, flash, url_for, redirect
 
 #instancia  de la aplicación web
 app=Flask(__name__, template_folder='templates')
-
+app.secret_key='12345'
 #Ruta raíz
 @app.route('/')
 
@@ -89,6 +93,52 @@ def reclutamiento():
 def avisolegal():
     ''' abre la pagina avisolegal'''
     return render_template('avisolegal.html')
+
+'''
+Parte funcional de la aplicación
+'''
+titulosOferta=[]
+descripcionesOferta=[]
+direccionesOferta=[]
+@app.route('/nuevaoferta',methods=['POST'])
+
+#función que permite enviar los datos ingresados hacia la tabla
+def nuevaoferta(): 
+    tituloOferta= request.form.get('titulo')
+    descripcionOferta= request.form.get('descripcion')
+    direccionOferta= request.form.get('direccion')
+  
+
+    #validar que se ingresen datos
+    if len(tituloOferta)>0 and len(descripcionOferta)>0 and len(direccionOferta)>0:
+       
+       #se ingresen los datos a las listas
+        titulosOferta.append(tituloOferta)
+        descripcionesOferta.append(descripcionOferta)
+        direccionesOferta.append(direccionOferta)
+          #con path se especifica la ruta del archivo
+        path, _=os.path.split(os.path.abspath(__file__))
+    
+        #se crean los arreglos 
+        data={}
+        data['titulosOferta']=[]
+        data['descripcionesOferta']=[]
+        data['direccionesOferta']=[]    
+        
+        data["titulosOferta"].append(titulosOferta)
+        data["descripcionesOferta"].append( descripcionesOferta)
+        data["direccionesOferta"].append(direccionesOferta)
+
+         #se escribe en el archivo datos.json
+        with open(path+f'/ofertas.json','w') as file:
+            json.dump(data, file, indent=4)
+
+        return render_template('ofertas.html', titulosOferta=titulosOferta, descripcionesOferta=descripcionesOferta, direccionesOferta=direccionesOferta)
+    else:
+        #alerta en caso de que existan errores
+        flash('Error al ingresar los datos')
+        return render_template('crearofertas.html')
+ 
 
 #Función principal que ejecuta la aplicación
 if __name__ == '__main__':
