28afe6b93dd20ed334e3f8ce7b605715f1f1a077
pluis9 <luispalcivar@gmail.com>
2022-06-05 22:48:15 -0500

    Se crean las demás páginas a utilizar

--- app.py
+++ app.py
@@ -14,17 +14,81 @@ def index():
 #Ruta registroempresa
 @app.route('/registroempresa')
 
-#Controlador quer renderiza la página principal
+#Controlador quer renderiza la página  registroempresa
 def registroempresa():
     ''' abre la pagina registroempresa'''
     return render_template('registroempresa.html')
 #Ruta registropostulante
 @app.route('/registropostulante')
 
-#Controlador quer renderiza la página principal
+#Controlador quer renderiza la página registropostulante
 def registropostulante():
     ''' abre la pagina registropostulante'''
     return render_template('registropostulante.html')
+
+#Ruta página accederempresa
+@app.route('/accederempresa')
+
+#Controlador quer renderiza la página accederempresa
+def accederempresa():
+    ''' abre la pagina accederempresa'''
+    return render_template('accederempresa.html')
+
+#Ruta página accederpostulante
+@app.route('/accederpostulante')
+
+#Controlador quer renderiza la página accederempresa
+def accederpostulante():
+    ''' abre la pagina accederpostulante'''
+    return render_template('accederpostulantea.html')
+
+#Ruta página crearoferta
+@app.route('/crearoferta')
+
+#Controlador quer renderiza la página crearoferta
+def crearoferta():
+    ''' abre la pagina crearoferta'''
+    return render_template('crearoferta.html')
+
+#Ruta página crearoferta
+@app.route('/ofertas')
+
+#Controlador quer renderiza la página ofertas
+def ofertas():
+    ''' abre la pagina ofertas'''
+    return render_template('ofertas.html')
+
+#Ruta página crearoferta
+@app.route('/nosotros')
+
+#Controlador quer renderiza la página ofertas
+def nosotros():
+    ''' abre la pagina nosotros'''
+    return render_template('nosotros.html')
+
+#Ruta página empresaslistado
+@app.route('/empresaslistado')
+
+#Controlador quer renderiza la página empresaslistado
+def empresaslistado():
+    ''' abre la pagina empresaslistado'''
+    return render_template('empresaslistado.html')
+
+#Ruta página reclutamiento
+@app.route('/reclutamiento')
+
+#Controlador quer renderiza la página reclutamiento
+def reclutamiento():
+    ''' abre la pagina reclutamiento'''
+    return render_template('reclutamiento.html')
+
+#Ruta página avisolegal
+@app.route('/avisolegal')
+
+#Controlador quer renderiza la página avisolegal
+def avisolegal():
+    ''' abre la pagina avisolegal'''
+    return render_template('avisolegal.html')
 
 #Función principal que ejecuta la aplicación
 if __name__ == '__main__':
